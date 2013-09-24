915c927e2ed67ba457ce7b49721694bd171d0ac2
gilvam <gilvamm@gmail.com>
2013-09-24 14:10:39 -0300

    Não permitir que sejam criados usuários com nome ou email em branco.

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+	# Attrs accessible
+	attr_accessible :name, :email, :age
+
+	# Validations
+	validates :name, presence:true, allow_blank:false
+	validates :email, presence:true, allow_blank:false
+
+	# Associations
+	# Scopes
+	# Públic methods
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130924170059) do
+
+  create_table "users", force: true do |t|
+    t.string   "name",       null: false
+    t.string   "email",      null: false
+    t.integer  "age"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
