c5ce3231deeee9479035104efcb96422d6106370
gilvam <gilvamm@gmail.com>
2013-09-24 14:21:45 -0300

    Adicionando campos gender no modelo User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
 	# Attrs accessible
 	attr_accessible :name, :email, :age
 
+	#Constants
+	MALE 		= 1
+	FEMALE	= 2
+	OTHER		= 3
+
 	# Validations
 	validates :name, presence:true, allow_blank:false
 	validates :email, presence:true, allow_blank:false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130924170059) do
+ActiveRecord::Schema.define(version: 20130924171351) do
 
   create_table "users", force: true do |t|
     t.string   "name",       null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130924170059) do
     t.integer  "age"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "gender"
   end
 
 end
